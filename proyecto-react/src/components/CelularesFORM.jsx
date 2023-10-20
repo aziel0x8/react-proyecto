7c0d9a6f5eafcc666c4021b685f1b471f2b67917
GabrielSolorzanoD <gabriel.solorzano.05@gmail.com>
2023-10-20 15:58:16 -0300

    scrum-21

--- proyecto-react/src/components/CelularesFORM.jsx
+++ proyecto-react/src/components/CelularesFORM.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CelularesFORM(){
 
@@ -8,7 +9,7 @@ function CelularesFORM(){
             <div>
                
                 <h1 className="text-white text-center pt-3">Tienda de Celulares</h1>
-                <button className="btn btn-dark btn-lg rounded-0 rounded-end-4">Ir a Tienda de Celulares</button>
+                <Link to={"/celulares"} className="btn btn-dark btn-lg rounded-0 rounded-end-4">Ir a Tienda de Celulares</Link>
                 <h3 className="text-white text-center mt-4 mb-3">Ingreso de Inventario</h3>
             </div>
             <div className="bg-dark position-relative pt-4 rounded-3" style={{height: "400px", width: "55%", marginLeft: "22%"}} >
--- proyecto-react/src/components/VideoJuegosFORM.jsx
+++ proyecto-react/src/components/VideoJuegosFORM.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function VideoJuegosFORM(){
 
@@ -7,7 +8,7 @@ function VideoJuegosFORM(){
             
             <div>
                 <h1 className="text-white text-center pt-3">Tienda de Video Juegos</h1>
-                <button className="btn btn-danger btn-lg rounded-0 rounded-end-4">Ir a Tienda de Video Juegos</button>
+                <Link to={"/viedeoJuegos"} className="btn btn-danger btn-lg rounded-0 rounded-end-4">Ir a Tienda de Video Juegos</Link>
                 <h3 className="text-white text-center mt-4 mb-3">Ingreso de Inventario</h3>
             </div>
             <div className="bg-danger position-relative pt-4 rounded-3" style={{height: "330px", width: "55%", marginLeft: "22%"}} >
