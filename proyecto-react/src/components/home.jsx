079ad2d1b9bde31fe362126538b55124dbef3341
aziel0x8 <aziel1966@gmail.com>
2023-10-22 12:55:20 -0600

    link

--- proyecto-react/src/components/home.jsx
+++ proyecto-react/src/components/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home(){
 
@@ -7,7 +8,9 @@ function Home(){
             <div className="bg-danger d-inline-block w-50 position-relative m-0 p-0" style={{height: "100%"}}>
                 <div className="position-relative top-50 start-50 translate-middle text-center">
                     <h1 className="text-white ">Tienda de Celulares</h1>
-                    <button className="btn btn-dark mt-5">Ir a la Tienda</button>
+                    {/* <button ></button> */}
+                    <Link to={"/celulares"} className="btn btn-dark mt-5">Ir a la Tienda</Link>
+                    
                 </div>
                 
             </div>
@@ -15,7 +18,8 @@ function Home(){
             <div className="bg-dark d-inline-block w-50 position-relative m-0 p-0" style={{height: "100%"}}>
                 <div className="position-relative top-50 start-50 translate-middle text-center">
                     <h1 className="text-white ">Tienda de Video Juegos</h1>
-                    <button className="btn btn-danger mt-5">Ir a la Tienda</button>
+                    {/* <button className="btn btn-danger mt-5">Ir a la Tienda</button> */}
+                    <Link to={"/viedeoJuegos"} className="btn btn-dark mt-5">Ir a la Tienda</Link>
                 </div> 
             </div>
         </div>
