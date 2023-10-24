7b01ccf1e8e51809fdeb1c9665fbf6442f386b1f
borishdez <borisnandez93@gmail.com>
2023-10-24 00:47:53 -0600

    correccion en ruta para agregar nuevo registro

--- proyecto-react/src/components/VideoJuegosCRUD.jsx
+++ proyecto-react/src/components/VideoJuegosCRUD.jsx
@@ -14,7 +14,7 @@ function VideoJuegosCRUD() {
             <div className="container-fluid">
 
                 <h1 className="text-white text-center pt-3">Tienda de Video Juegos</h1>
-                <Link to={"/videoJuegosFORM"} className="btn btn-danger btn-lg rounded-0 rounded-end-4 mb-4">Ingresar nuevo registro</Link>
+                <Link to={"/viedeoJuegos/add"} className="btn btn-danger btn-lg rounded-0 rounded-end-4 mb-4">Ingresar nuevo registro</Link>
                 <h3 className="text-white text-center mt-4">Consultar Inventario</h3>
 
                 <div className="col-md-10 mx-auto mt-4">
@@ -41,8 +41,8 @@ function VideoJuegosCRUD() {
                                         <td>{item.Precio}</td>
                                         <td>{item.Categoria}</td>
                                         <td>
-                                            <Link to={"/videoJuegosFORM"} className="btn btn-danger btn-sm mr-2">Eliminar</Link>
-                                            <Link to={"/videoJuegosFORM"} className="btn btn-secondary btn-sm">Editar</Link>
+                                            <Link to={`/viedeoJuegos/delete/${Object.values(item)[0]}`} className="btn btn-danger btn-sm mr-2">Eliminar</Link>
+                                            <Link to={`/viedeoJuegos/edit/${Object.values(item)[0]}`} className="btn btn-secondary btn-sm">Editar</Link>
                                         </td>
                                     </tr>
                                 ))}
