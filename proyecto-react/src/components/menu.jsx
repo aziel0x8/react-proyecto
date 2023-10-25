db044c5da9a9a2c20d40b81db54910ad5d7243a6
borishdez <borisnandez93@gmail.com>
2023-10-24 19:53:51 -0600

    link kodiTeam a home

--- proyecto-react/src/components/menu.jsx
+++ proyecto-react/src/components/menu.jsx
@@ -14,7 +14,7 @@ function Menu(){
 
             <nav className="navbar navbar-expand-lg text-white bg-dark bg-gradient">
                 <div className="container-fluid">
-                <h2>KodiTeam</h2>
+                    <Link to={"/"} className="nav-link text-white"><h2>KodiTeam</h2></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
